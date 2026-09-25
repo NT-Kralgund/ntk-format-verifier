0237fd40408d3848eb6e940e450ae4dca8180fcd
NT-Kralgund <ntofficeqaziabad@gmail.com>
2026-09-25 19:12:38 +0530

    Add files via upload

---
+++ README_HOSTING.txt
@@ -0,0 +1,1 @@
+Upload index.html and config.js to a public static web host. Then use the resulting URL (ending in /) in the desktop generator. No Google Sheet is required.
---
+++ config.js
@@ -0,0 +1,2 @@
+window.NTK_PUBLIC_KEY_B64 = "2yRQY7bnQVQWWg6k3Er3uUROLeQnqOKOE1ZyaCtSb30";
+window.NTK_OFFICE = "Government of J&K | Department of Revenue | OFFICE OF NAYIB TEHSILDAR KRALGUND";
